f183872dd7b658a584636d943706683284c32ef9
Zeke-Cachi <ezequielcachi@gmail.com>
2023-11-14 16:03:17 -0300

    debuggin cors

--- app.ts
+++ app.ts
@@ -3,5 +3,5 @@ import Database from "./db/db.js";
 import Server from "./server.js";
 //-------------------------------------------------------------------
 
-Database.connect();
 new Server();
+Database.connect();
