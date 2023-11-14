cda3fa9d010063b934ddf88f603bd291fbb2b19f
Zeke-Cachi <ezequielcachi@gmail.com>
2023-11-14 15:30:52 -0300

    CORS

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ class Server {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(morgan("dev"));
     const corsConfig = {
-      origin: "https://generic-ecommerce-five.vercel.app/",
+      origin: "https://generic-ecommerce-five.vercel.app",
       methods: "GET, POST, PUT, DELETE",
       allowedHeaders: "Content-type, Authorization",
       credentials: true,
