e6807eb063eb556dfcafdd931647be7ce348dd0b
Zeke-Cachi <ezequielcachi@gmail.com>
2023-11-14 15:19:15 -0300

    Still changing CORS config

--- server.ts
+++ server.ts
@@ -21,11 +21,20 @@ class Server {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(morgan("dev"));
     const corsConfig = {
-      origin: "https://generic-ecommerce-five.vercel.app",
-      headers: "*",
+      origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+      ) => {
+        const allowedOrigins = ["https://generic-ecommerce-five.vercel.app"];
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       methods: "GET,HEAD,PUT,POST,DELETE,OPTIONS",
+      allowedHeaders: "Content-Type, Authorization",
       credentials: true,
-      allowedHeaders: "Content-type, Authorization",
       optionsSuccessStatus: 200,
     };
     this.app.use(cors(corsConfig));
